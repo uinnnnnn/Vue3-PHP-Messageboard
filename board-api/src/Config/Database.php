7db5ca8d551c859ed31afd409ad91a2a2d6f170a
inru <sumi143yy@gmail.com>
2023-11-28 18:39:14 +0800

    feat: init

---
+++ board-api/src/Config/Database.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Config;
+
+class Database
+{
+    public const DATABASE_INFO = [
+        'db_type' => 'mysql',
+        'db_host' => 'localhost',
+        'db_name' => 'fortest',
+        'db_user' => 'root',
+        'db_pass' => ''
+    ];
+}
